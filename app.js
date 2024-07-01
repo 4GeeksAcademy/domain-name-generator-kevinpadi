01ad7fd85157aea4eb6dc439394d3e658ae54e5e
Kevin Padilla <kevpadilla01@gmail.com>
2024-07-01 13:37:04 +0000

    projecto finalizado

---
+++ app.js
@@ -0,0 +1,11 @@
+let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+for (let index = 0; index < pronoun.length; index++) {
+    for (let a = 0; a < adj.length; a++) {
+        for (let n = 0; n < noun.length; n++) {
+            console.log(pronoun[index]+adj[a]+noun[n]+'.com')
+        }
+    }
+}
